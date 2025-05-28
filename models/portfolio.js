6a10909f458359e15324b11deaabba080106d752
02fe22bcs177 <131421979+02fe22bcs177@users.noreply.github.com>
2025-05-28 19:57:55 +0530

    Update portfolio.js

--- models/portfolio.js
+++ models/portfolio.js
@@ -1,1 +1,31 @@
+let portfolios = [];
+let idCounter = 1;
+
+function getAll() {
+  return portfolios;
+}
+
+function getById(id) {
+  return portfolios.find(p => p.id === parseInt(id));
+}
+
+function create({ name, role, photo }) {
+  const newPortfolio = { id: idCounter++, name, role, photo };
+  portfolios.push(newPortfolio);
+  return newPortfolio;
+}
+
+function update(id, data) {
+  const index = portfolios.findIndex(p => p.id === parseInt(id));
+  if (index !== -1) {
+    portfolios[index] = { ...portfolios[index], ...data };
+  }
+  return portfolios[index];
+}
+
+function remove(id) {
+  portfolios = portfolios.filter(p => p.id !== parseInt(id));
+}
+
+module.exports = { getAll, getById, create, update, remove };
 
